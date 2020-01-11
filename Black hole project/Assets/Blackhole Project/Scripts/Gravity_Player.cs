7ff9d5fc57319526976cf7c82b83fe8ca65d9bab
Antoine Doyon <antoinedoyondelisle@gmail.com>
2020-01-11 17:39:44 -0500

    New Gravity

--- Black hole project/Assets/Blackhole Project/Scripts/Gravity_Player.cs
+++ Black hole project/Assets/Blackhole Project/Scripts/Gravity_Player.cs
@@ -2,35 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gravity : MonoBehaviour
+public class Gravity_Player : MonoBehaviour
 {
     #region VARIABLES
     public float speed = 0.5f;
     private Transform target;
     Animator Anim;
+    Animator Explosion_Anim;
+    //ShieldHP
     #endregion
 
-    void Start ()
+    void Start()
     {
         target = GameObject.FindGameObjectWithTag("BlackHole").GetComponent<Transform>();
         Anim = GetComponent<Animator>();
+        Explosion_Anim = GameObject.FindGameObjectWithTag("Explosion").GetComponent<Animator>();
     }
 
-    // Objects Move toward Blackhole
+   
     void Update()
     {
+        // Objects Move toward Blackhole
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        // Explosion Game OVER
+        /*if(ShieldHP == 0)
+            {
+               GetComponent<SpriteRenderer>().enabled = false;
+               Explosion_Anim.SetBool("IsExploding", true);
+            }
+       */
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        // Gravity slowed and reduce object
+        // Black hole Game Over
         if (col.gameObject.tag == "Gravity_attraction")
         {
             speed = 0.2f;
             Debug.Log("Gravity_attraction");
             Anim.SetBool("IsReducing", true);
+            GetComponent<PlayerMovement>().enabled = false;
         }
+
+        if (col.gameObject.tag == "Gravity_attraction2")
         {
             speed = speed + 1;
         }
@@ -40,6 +55,19 @@ public class Gravity : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        // Collision with Planet
+        if (col.gameObject.tag == "Planet")
+        {
+           
+        }
+
+        // Collision with Meteor
+        if (col.gameObject.tag == "Meteor")
+        {
+
+        }
+
     }
 
 }
