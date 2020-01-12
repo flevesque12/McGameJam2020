c945b10b02bbef918f8f9ba01797ba74aa31874b
Frederic Levesque <flevesque124@gmail.com>
2020-01-12 09:29:19 -0500

    MainMenu Naviguation script added and scene. Mothership sprit added

---
+++ Black hole project/Assets/Blackhole Project/FredScripts/MenuNavigation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuNavigation : MonoBehaviour
+{
+    public GameObject creditPanel;
+    public GameObject mainPanel;
+    
+    public void GotoMainGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void ActivateCreditPanel()
+    {
+        creditPanel.SetActive(true);
+        mainPanel.SetActive(false);
+    }
+
+    public void ActivateMainPanel()
+    {
+        creditPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+}
